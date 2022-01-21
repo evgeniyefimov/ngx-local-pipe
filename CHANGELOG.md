e9c5e9b25824f248d6c464eb07d6b180e9266736
evgeniyefimov <x_jenka_x@inbox.ru>
2022-01-21 12:01:12 +0300

    feat: update Angular packages to version 13.0.0 feat: release v2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,23 @@
+# 2.0.0
+
+- **feat** update Angular packages to version 13.0.0
+
+### BREAKING CHANGES
+
+- **chore** changed an order of arguments
+
+BEFORE:
+
+```
+firstArgument | ngxLocalPipe : componentMethod : secondArgument : thirdArgument ...etc
+```
+
+AFTER:
+
+```ts
+componentMethod | ngxLocalPipe : firstArgument : secondArgument : thirdArgument ...etc
+```
+
 # 1.0.1
 
 - **chore** support angular 12
--- projects/ngx-local-pipe/src/lib/ngx-local-pipe.pipe.ts
+++ projects/ngx-local-pipe/src/lib/ngx-local-pipe.pipe.ts
@@ -6,11 +6,10 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class NgxLocalPipe implements PipeTransform {
   // Utilized variadic tuple types for types
   // https://www.typescriptlang.org/docs/handbook/release-notes/typescript-4-0.html#variadic-tuple-types
-  public transform<Value, Args extends unknown[], Result>(
-    value: Value,
-    func: (value: Value, ...args: Args) => Result,
+  public transform<Args extends unknown[], Result>(
+    func: (...funcargs: Args) => Result,
     ...args: Args
   ): Result {
-    return func(value, ...(args || []));
+    return func(...args);
   }
 }
